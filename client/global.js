3a62b62791e9f166633e6fff8fb64c3bd52aca17
tim <tjhickey@brandeis.edu>
2015-10-04 18:28:16 -0400

    fixed DNS issues

--- client/global.js
+++ client/global.js
@@ -1,3 +1,5 @@
 Template.registerHelper('formatDate', function(date) {
   return moment(date).format('MM-DD-YYYY');
 });
+
+Meteor.startup(function () {  Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://leiner.cs-i.brandeis.edu:4250/'; });
