e43d427f708d0afd447e2595ae74565e98728299
tim <tjhickey@brandeis.edu>
2015-10-04 19:09:26 -0400

    setup to use google authentication

--- client/global.js
+++ client/global.js
@@ -1,5 +1,3 @@
 Template.registerHelper('formatDate', function(date) {
   return moment(date).format('MM-DD-YYYY');
 });
-
-Meteor.startup(function () {  Meteor.absoluteUrl.defaultOptions.rootUrl = 'http://leiner.cs-i.brandeis.edu:4250/'; });
